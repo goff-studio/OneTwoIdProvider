4b8eb909c79b42df9497f68b1df885513646e35c
Alireza Goudarzi <alireza@goudarzi.ws>
2023-12-13 10:02:30 +0100

    openDeepLink optimiser

--- OneTwoIdProvider.podspec
+++ OneTwoIdProvider.podspec
@@ -1,12 +1,12 @@
 Pod::Spec.new do |s|
   s.name             = 'OneTwoIdProvider'
-  s.version          = '1.0.1'
+  s.version          = '1.0.4'
   s.summary          = 'A simple network request pod.'
   s.description      = 'A pod for making a simple network request and formatting the response as JSON.'
   s.homepage         = 'https://github.com/goff-studio/OneTwoIdProvider.git'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Your Name' => 'your.email@example.com' }
-  s.source           = { :git => 'https://github.com/goff-studio/OneTwoIdProvider.git', :tag => '1.0.1' }
+  s.source           = { :git => 'https://github.com/goff-studio/OneTwoIdProvider.git', :tag => '1.0.4' }
   s.swift_version    = '5.5'
   s.ios.deployment_target = '13.0'
   s.source_files = 'Sources/**/*.swift'
--- Sources/OneTwoIdProvider/OneTwoIdProvider.swift
+++ Sources/OneTwoIdProvider/OneTwoIdProvider.swift
@@ -4,15 +4,35 @@ import Alamofire
 public struct OneTwoIdProvider {
     public private(set) var text = "Hello, World!"
 
-        public init() {
-        }
-        public static func makeGetRequest(completion: @escaping (Result<String, Error>) -> Void) {
-            guard let url = URL(string: "https://swapi.dev/api/") else {
-                completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
-                return
-            }
+    public init() {
+    }
 
-            AF.request(url)
+    public static func loginWith12ID() {
+        print("Login with 12ID")
+    }
+
+    public static func openDeepLink(urlString: String) {
+        if let url = URL(string: urlString), UIApplication.shared.canOpenURL(url) {
+            // Open the app using the deep link
+            UIApplication.shared.open(url, options: [:]) { success in
+                if success {
+                    print("Opened \(urlString) successfully")
+                } else {
+                    print("Failed to open \(urlString)")
+                }
+            }
+        } else {
+            print("Invalid or unsupported deep link: \(urlString)")
+        }
+    }
+
+    public static func makeGetRequest(completion: @escaping (Result<String, Error>) -> Void) {
+        guard let url = URL(string: "https://swapi.dev/api/") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
+            return
+        }
+
+        AF.request(url)
                 .response { response in
                     switch response.result {
                     case .success(let data):
@@ -24,7 +44,7 @@ public struct OneTwoIdProvider {
                                 completion(.success(jsonString))
                             } else {
                                 completion(.failure(NSError(domain: "Unable to convert data to JSON string", code: 0, userInfo:
-    nil)))
+                                nil)))
                             }
                         } catch {
                             completion(.failure(error))
@@ -33,5 +53,5 @@ public struct OneTwoIdProvider {
                         completion(.failure(error))
                     }
                 }
-        }
+    }
 }
